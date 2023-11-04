07e603bf953d3cdac7c941be2c8f627bcdbdc22d
daniil-kuzmenko <daniil-kuzmenko@integlab.ru>
2023-11-04 15:35:32 +0300

    add tailwind and start to add FSD

--- src/app/index.tsx
+++ src/app/index.tsx
@@ -1,17 +1,15 @@
 import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import 'app/index.css';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
+    <div className="bg-black">
+      <header className="bg-amber-200 font-bold caret-amber-50">
+        <p className='font-bold accent-purple-700'>
           Edit <code>src/App.tsx</code> and save to reload.
         </p>
         <a
-          className="App-link"
+          className="text-purple-600"
           href="https://reactjs.org"
           target="_blank"
           rel="noopener noreferrer"
